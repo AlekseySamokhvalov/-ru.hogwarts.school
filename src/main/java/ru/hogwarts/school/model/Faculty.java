664a785f5eb5766c567842102ecff0a3f4ee8357
AlekseySamokhvalov <6560@mail.ru>
2023-11-24 07:35:21 +0300

    В модели добавлены аннотации @Entity, @Id и @GeneratedValue. Создан пакет repository. В нем находятся два интерфейса: StudentRepository и FacultyRepository.

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -1,9 +1,14 @@
 package ru.hogwarts.school.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
-
+@Entity
 public class Faculty {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private static int ID_COUNTER = 0;
 
     private Long id;
--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -1,9 +1,14 @@
 package ru.hogwarts.school.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
-
+@Entity
 public class Student {
-
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
