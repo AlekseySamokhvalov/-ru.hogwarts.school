bb1e67f2c6f23d0257881d418557c84ebe1624dd
AlekseySamokhvalov <6560@mail.ru>
2023-12-16 13:47:21 +0300

    Подключена зависимость liquibase-core. Создан пустой файл миграций. Настроен файл changelog-master.yml.В файле миграций созданы два changeset с SQL-командами для создания индексов.

--- src/main/java/ru/hogwarts/school/model/Avatar.java
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.Arrays;
 import java.util.Objects;
@@ -15,7 +17,8 @@ public class Avatar {
     private long fileSize;
 
     private String mediaType;
-
+    //    @Lob
+    @JsonIgnore
     private byte[] data;
 
     @OneToOne
